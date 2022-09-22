b6157e347c696028b2552dec1d1776f33a37dd92
mahmoudjameel <mahmoudjameel@mahmoudjameels-MacBook-Pro.local>
2022-09-22 12:14:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+ import PushNotificationIOS from "@react-native-community/push-notification-ios";
+ import PushNotification from "react-native-push-notification"; 
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+PushNotification.configure({
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+
+    // process the notification
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+  requestPermissions: Platform.OS === 'ios'
+
+    },
+    );
+  
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/screens/Home.js
@@ -0,0 +1,241 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Alert,
+    TextInput,
+    FlatList,
+    TouchableOpacity
+
+} from 'react-native';
+import CustomButton from '../utils/CustomButton';
+import GlobalStyle from '../utils/GlobalStyle';
+import SQLite from 'react-native-sqlite-storage';
+import { useSelector, useDispatch } from 'react-redux';
+import { setName, setAge, increaseAge, getCities } from '../redux/actions';
+import PushNotification from "react-native-push-notification";
+
+const db = SQLite.openDatabase(
+    {
+        name: 'MainDB',
+        location: 'default',
+    },
+    () => { },
+    error => { console.log(error) }
+);
+
+export default function Home({ navigation, route }) {
+
+    const {name, age, cities} = useSelector(state => state.userReducer);
+    const dispatch = useDispatch();
+
+    // const [name, setName] = useState('');
+    // const [age, setAge] = useState('');
+
+    useEffect(() => {
+        getData();
+        dispatch(getCities());
+    }, []);
+
+    const handleNotification = (item, index) =>{
+
+        PushNotification.cancelAllLocalNotifications();
+
+        PushNotification.localNotification({
+            channelId: "Test-Channel",
+            title: "You clicked on " + item.country,
+            message: item.city,
+            bigText: item.city + " is one of the largest and most beatiful cities in " + item.country,
+            color: "red",
+            id: index
+        });
+
+        PushNotification.localNotificationSchedule({
+            channelId: "test-channel",
+            title: "Alarm",
+            message: "You clicked on " + item.country + " 20 seconds ago",
+            date: new Date(Date.now() + 20 * 1000),
+            allowWhileIdle: true,
+        });
+    }
+
+    const getData = () => {
+        try {
+            // AsyncStorage.getItem('UserData')
+            //     .then(value => {
+            //         if (value != null) {
+            //             let user = JSON.parse(value);
+            //             setName(user.Name);
+            //             setAge(user.Age);
+            //         }
+            //     })
+            db.transaction((tx) => {
+                tx.executeSql(
+                    "SELECT Name, Age FROM Users",
+                    [],
+                    (tx, results) => {
+                        var len = results.rows.length;
+                        if (len > 0) {
+                            var userName = results.rows.item(0).Name;
+                            var userAge = results.rows.item(0).Age;
+                            // setName(userName);
+                            // setAge(userAge);
+                            dispatch(setName(userName));
+                            dispatch(setAge(userAge));
+
+                        }
+                    }
+                )
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const updateData = async () => {
+        if (name.length == 0) {
+            Alert.alert('Warning!', 'Please write your data.')
+        } else {
+            try {
+                // var user = {
+                //     Name: name
+                // }
+                // await AsyncStorage.mergeItem('UserData', JSON.stringify(user));
+                db.transaction((tx) => {
+                    tx.executeSql(
+                        "UPDATE Users SET Name=?",
+                        [name],
+                        () => { Alert.alert('Success!', 'Your data has been updated.') },
+                        error => { console.log(error) }
+                    )
+                })
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+    const removeData = async () => {
+        try {
+            // await AsyncStorage.clear();
+            db.transaction((tx) => {
+                tx.executeSql(
+                    "DELETE FROM Users",
+                    [],
+                    () => { navigation.navigate('Login') },
+                    error => { console.log(error) }
+                )
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <View style={styles.body}>
+            <Text style={[
+                GlobalStyle.CustomFont,
+                styles.text
+            ]}>
+                Welcome {name} !
+            </Text>
+            
+            <FlatList
+           data={cities}
+            renderItem={({item, index})=>(
+                <TouchableOpacity
+                onPress={() => { handleNotification(item, index) }}
+                >
+                <View style={styles.item}>
+                    <Text  style={styles.title} >{item.country}</Text>
+                    <Text  style={styles.subtitle} >{item.city}</Text>
+                </View>
+                </TouchableOpacity>
+
+            )}
+            keyextractor={(item, index) => index.toString()}
+
+            
+            
+            />
+
+
+
+
+            
+            {/* <Text style={[
+                GlobalStyle.CustomFont,
+                styles.text
+            ]}>
+                Your age is {age}
+            </Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Enter your name'
+                value={name}
+                onChangeText={(value) => dispatch(setName(value))}
+            />
+            <CustomButton
+                title='Update'
+                color='#ff7f00'
+                onPressFunction={updateData}
+            />
+                        <CustomButton
+                title='Increase Age'
+                color='#f2e'
+                onPressFunction={()=>{dispatch(increaseAge())}}
+            />*/}
+
+            <CustomButton
+                title='Remove'
+                color='#f40100'
+                onPressFunction={removeData}
+            /> 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 40,
+        margin: 10,
+    },
+    input: {
+        width: 300,
+        borderWidth: 1,
+        borderColor: '#555',
+        borderRadius: 10,
+        backgroundColor: '#ffffff',
+        textAlign: 'center',
+        fontSize: 20,
+        marginTop: 130,
+        marginBottom: 10,
+    },
+    item:{
+        backgroundColor:'#ffffffff',
+        borderWidth:2,
+        borderColor:'#cccc',
+        margin:7,
+        width:330,
+        alignItems:'center',
+        justifyContent:'center'
+   
+
+    },
+    title:{
+       fontSize:30,
+       margin:10
+    },
+    subtitle:{
+        fontSize:20,
+        margin: 10,
+        color:'#999999'
+
+    }
+})
